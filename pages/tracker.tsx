b90c998186f74e3783eec43ea57ea94a05168190
Alice Alsford <alice.alsford@mobi2go.com>
2022-11-14 13:26:13 +1300

    Add progress bar

--- pages/tracker.tsx
+++ pages/tracker.tsx
@@ -1,25 +1,32 @@
 import styles from "../styles/Times.module.css";
 import { useState, useEffect } from "react";
+import { Line } from "rc-progress";
 
 export default function Tracker() {
-  const [time, setTime] = useState(new Date());
+  const [currentTime, setCurrentTime] = useState(new Date());
   const [timeDiff, setTimeDiff] = useState(Number);
-
-  let currentDate = new Date().toJSON().slice(0, 10);
-  const wakeupTime = new Date(currentDate + " 16:38:00");
+  const [percentageTime, setPercentageTime] = useState(Number);
+  const [wakeupTime, setWakeupTime] = useState({});
 
   const getMinsDiff = (startDate, endDate) => {
     const msInMins = 1000 * 60;
 
-    return Math.round(Math.abs(endDate - startDate) / msInMins);
+    return Math.round((endDate - startDate) / msInMins);
   };
 
   useEffect(() => {
-    setTimeDiff(getMinsDiff(wakeupTime, time));
+    let currentDate = new Date().toJSON().slice(0, 10);
+
+    setWakeupTime(new Date(currentDate + " 13:00:00"));
   }, []);
 
   useEffect(() => {
-    var timer = setInterval(() => setTime(new Date()), 1000);
+    setTimeDiff(getMinsDiff(wakeupTime, currentTime));
+    setPercentageTime(Math.floor((timeDiff / 90) * 100));
+  }, [currentTime]);
+
+  useEffect(() => {
+    let timer = setInterval(() => setCurrentTime(new Date()), 1000);
     return function cleanup() {
       clearInterval(timer);
     };
@@ -28,6 +35,7 @@ export default function Tracker() {
   return (
     <div className={styles.container}>
       <p>Time since: {timeDiff}</p>
+      <Line percent={percentageTime} strokeWidth={4} strokeColor="blue" />
     </div>
   );
 }
